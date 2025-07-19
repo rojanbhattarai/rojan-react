cb9fde1cc7dfaea6f8822e2a8fdc667c7e3ada92
rojanbhattarai <bhattarairoj301@gmail.com>
2025-07-19 21:16:02 +0545

    initial commit

---
+++ vite-project/src/pages/Home.jsx
@@ -0,0 +1,54 @@
+import { useEffect ,useState} from "react";
+import Navbar from "./components/Navbar"
+import Cards from "./components/cards"
+import axios from 'axios'
+function Home(){
+    const[blogs,setBlogs]=useState([]);
+
+    //--->
+    async function fetchBlogs(){
+     const response=  await axios.get("https://687af47babb83744b7ee4d60.mockapi.io/Blogs")
+    
+    
+     if(response.status=200){
+ setBlogs(response.data);
+     }
+     else{
+        alert("Error!!!")
+     }
+    
+
+    }
+
+    //--->
+// call back and anonymus functions to learn
+    useEffect(()=>{
+fetchBlogs()
+    },[])
+
+    console.log(blogs,"this is blogs")
+    return (
+
+    <>
+
+
+<>
+<Navbar /> 
+<div className="flex justify-between flex-wrap">
+    {blogs.map(function(blog){
+return(
+
+<Cards key={blog.id} blog={blog} /> 
+)
+    })}
+ </div> 
+</>
+
+
+    </>
+
+
+    )
+}
+
+export default Home
---
+++ vite-project/src/pages/components/Cards.jsx
@@ -0,0 +1,34 @@
+
+function Cards({blog}) {
+  console.log(blog)
+  return (
+    <div className="max-w-screen-xl mx-auto p-6 sm:p-10 md:p-16">
+      <div className="max-w-md mx-auto bg-white shadow-xl rounded-3xl overflow-hidden transform transition duration-300 hover:scale-105 hover:shadow-2xl">
+        <a href="/singlepage">
+          <img
+            className="w-full h-56 object-cover"
+            src={blog.image}
+            alt="React Blog Post"
+          />
+        </a>
+        <div className="p-6 bg-purple-700 text-white">
+          <a
+            href="/singlepage"
+            className="text-xl font-semibold hover:text-black block mb-2"
+          >
+           {blog.title}
+          </a>
+          <p className="text-sm leading-relaxed">
+           {blog.subtitle}
+          </p>
+          <div className="mt-4 text-xs text-gray-300">
+            By <span className="font-medium text-white">Rojan Bhattarai</span> |
+            Location: <span className="text-white">Gothgaun, Nepal</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
